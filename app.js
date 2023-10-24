1d1740cc4021506b0f1df44b978cf4fbd195a2a5
Matija <matijac2k@gmail.com>
2023-10-24 14:00:22 +0200

    Build function that adds mark to array[index]

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const boxes = document.querySelectorAll(".item-box");
 
 const Gameboard = (function () {
-  return { gameboard: ["X", "O", "X", "O", "X", "O", "X", "O", "X"] };
+  let gameboard = [];
+  mark = "O";
+  const switchMark = () => {
+    if (mark === "X") {
+      mark = "O";
+    } else {
+      mark = "X";
+    }
+    return mark;
+  };
+  const addToArray = (index, mark) => {
+    gameboard[index] = mark;
+  };
+
+  addToArray(0, switchMark());
+
+  return {
+    gameboard,
+    addToArray,
+  };
 })();
 
 function players(name) {
   return { name };
 }
+
+const game = {};
 
 function renderArrayContents() {
   const items = Gameboard.gameboard; //array
