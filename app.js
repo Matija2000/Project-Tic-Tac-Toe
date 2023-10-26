7a5d79a730f90807985254a88a1569be95a11092
Matija <matijac2k@gmail.com>
2023-10-26 16:15:04 +0200

    Build game logic

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const Gameboard = (function () {
   const addToArray = (index, mark) => {
     gameboard[index] = mark;
     renderArrayContents();
+    game.checkForWinner();
   };
 
   function renderArrayContents() {
@@ -40,6 +41,31 @@ function players(name) {
   return { name };
 }
 
-const game = {};
+const game = (function () {
+  let board = Gameboard.gameboard;
 
-const lol = document.querySelector("#lol");
+  const checkForWinner = () => {
+    if (
+      (board[0] == "X" && board[1] == "X" && board[2] == "X") ||
+      (board[3] == "X" && board[4] == "X" && board[5] == "X") ||
+      (board[6] == "X" && board[7] == "X" && board[8] == "X") ||
+      (board[0] == "O" && board[1] == "O" && board[2] == "O") ||
+      (board[3] == "O" && board[4] == "O" && board[5] == "O") ||
+      (board[6] == "O" && board[7] == "O" && board[8] == "O") ||
+      (board[0] == "X" && board[4] == "X" && board[8] == "X") ||
+      (board[2] == "X" && board[4] == "X" && board[6] == "X") ||
+      (board[0] == "O" && board[4] == "O" && board[8] == "O") ||
+      (board[2] == "O" && board[4] == "O" && board[6] == "O") ||
+      (board[0] == "X" && board[3] == "X" && board[6] == "X") ||
+      (board[1] == "X" && board[4] == "X" && board[7] == "X") ||
+      (board[2] == "X" && board[5] == "X" && board[8] == "X") ||
+      (board[0] == "O" && board[3] == "O" && board[6] == "O") ||
+      (board[1] == "O" && board[4] == "O" && board[7] == "O") ||
+      (board[2] == "O" && board[5] == "O" && board[8] == "O")
+    ) {
+      console.log("gg");
+    }
+  };
+
+  return { checkForWinner, board };
+})();
